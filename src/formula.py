c88fe3d14ee30e7512cce7a908eff6c50cfdad77
Francisco Maturana <francisco.matu@gmail.com>
2022-02-21 21:20:46 -0500

    Reformat code

--- src/formula.py
+++ src/formula.py
@@ -6,7 +6,7 @@ from scipy.special import binom
 
 
 def poisson_binomial_pmf(probabilities: np.ndarray) -> np.ndarray:
-    '''Poisson-Binomial probability mass function.
+    """Poisson-Binomial probability mass function.
 
     Args:
         probabilities (np.ndarray): Probability of success of each trial.
@@ -14,7 +14,7 @@ def poisson_binomial_pmf(probabilities: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: Sequence where $i$th element is probability of observing exactly $i$
         successful trials.
-    '''
+    """
     alpha = np.prod(probabilities)
     s = -(1 - probabilities) / probabilities
     coeffs = np.poly(s) * alpha
@@ -22,8 +22,10 @@ def poisson_binomial_pmf(probabilities: np.ndarray) -> np.ndarray:
     return coeffs
 
 
-def homogeneous_mttdl_formula(n: int, k: int, failure_rate: float, repair_rate: float) -> float:
-    '''Calculate homogeneous MTTDL via an approximate formula.
+def homogeneous_mttdl_formula(
+    n: int, k: int, failure_rate: float, repair_rate: float
+) -> float:
+    """Calculate homogeneous MTTDL via an approximate formula.
 
     Args:
         n (int): Length of MDS code.
@@ -33,15 +35,17 @@ def homogeneous_mttdl_formula(n: int, k: int, failure_rate: float, repair_rate: 
 
     Returns:
         float: approximate MTTDL.
-    '''
+    """
     nck = binom(n, k)
-    ratio = (failure_rate / repair_rate)**(n - k)
+    ratio = (failure_rate / repair_rate) ** (n - k)
     data_loss_rate = nck * ratio * failure_rate * k
     return 1 / data_loss_rate
 
 
-def mttdl_formula(n: int, k: int, failure_rates: np.ndarray, repair_rate: float) -> float:
-    '''Calculate MTTDL via an approximate formula.
+def mttdl_formula(
+    n: int, k: int, failure_rates: np.ndarray, repair_rate: float
+) -> float:
+    """Calculate MTTDL via an approximate formula.
 
     Args:
         n (int): Length of MDS code.
@@ -52,7 +56,7 @@ def mttdl_formula(n: int, k: int, failure_rates: np.ndarray, repair_rate: float)
 
     Returns:
         float: approximate MTTDL.
-    '''
+    """
     probabilities = repair_rate / (repair_rate + failure_rates)
     pmf = poisson_binomial_pmf(probabilities)
     data_loss_rate = repair_rate * (n - k + 1) * pmf[k - 1]
@@ -60,38 +64,40 @@ def mttdl_formula(n: int, k: int, failure_rates: np.ndarray, repair_rate: float)
 
 
 def _test_homogeneous_mttdl_formula():
-    '''Test that homogeneous and heterogenous formula give similar results.'''
+    """Test that homogeneous and heterogenous formula give similar results."""
     n = 14
     k = 10
     failure_rate = 4.57e-06
     failure_rates = np.array([failure_rate] * n)
-    repair_rate = 4.
-    homogeneous_mttdl = homogeneous_mttdl_formula(
-        n, k, failure_rate, repair_rate)
+    repair_rate = 4.0
+    homogeneous_mttdl = homogeneous_mttdl_formula(n, k, failure_rate, repair_rate)
     mttdl = mttdl_formula(n, k, failure_rates, repair_rate)
     print(
-        f'homogeneous_MTTDL(n={n}, k={k}, failure rate={failure_rate}, '
-        f'repair rate={repair_rate}): {homogeneous_mttdl}')
+        f"homogeneous_MTTDL(n={n}, k={k}, failure rate={failure_rate}, "
+        f"repair rate={repair_rate}): {homogeneous_mttdl}"
+    )
     print(
-        f'MTTDL(n={n}, k={k}, failure rates={failure_rates}, '
-        f'repair rate={repair_rate}): {mttdl}')
+        f"MTTDL(n={n}, k={k}, failure rates={failure_rates}, "
+        f"repair rate={repair_rate}): {mttdl}"
+    )
     assert abs(np.log(homogeneous_mttdl) - np.log(mttdl)) < 1
 
 
 def _test_mttdl_formula():
-    '''Test that MTTDL formula gives sane results.'''
+    """Test that MTTDL formula gives sane results."""
     n = 14
     k = 10
     failure_rates = np.array([2.07e-06] * 7 + [4.57e-06] * 7)
-    repair_rate = 4.
+    repair_rate = 4.0
     mttdl = mttdl_formula(n, k, failure_rates, repair_rate)
     print(
-        f'MTTDL(n={n}, k={k}, failure rates={failure_rates}, '
-        f'repair rate={repair_rate}): {mttdl}')
+        f"MTTDL(n={n}, k={k}, failure rates={failure_rates}, "
+        f"repair rate={repair_rate}): {mttdl}"
+    )
     assert abs(np.log(mttdl) - np.log(7e25)) < 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run tests.
     _test_mttdl_formula()
     _test_homogeneous_mttdl_formula()
